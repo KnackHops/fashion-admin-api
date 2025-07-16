07b7bb21d4ef395404e1805a5ac5bd846d70c601
Aldrian <aldrian.telan@gmail.com>
2025-07-17 00:20:54 +0800

    all exceptions

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './all-exceptions';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'fatal'],
+  });
   app.enableCors();
   app.setGlobalPrefix('admin/api');
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
   await app.listen(process.env.PORT ?? 3001);
 }
