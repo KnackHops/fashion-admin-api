6844510d3f2fa2bbd8d23d086248aeec1716b53e
Aldrian <aldrian.telan@gmail.com>
2025-08-04 23:28:48 +0800

    fixing deployment bug

--- src/modules/item/item.controller.ts
+++ src/modules/item/item.controller.ts
@@ -33,7 +33,7 @@ export class ItemController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.itemService.findOne(+id);
+    return this.itemService.findOne(id);
   }
 
   // @Patch(':id')
