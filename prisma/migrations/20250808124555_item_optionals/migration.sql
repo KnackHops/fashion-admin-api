3d7becb4972a8bd7fd9b3ef8b593c20be34b61cf
Aldrian <aldrian.telan@gmail.com>
2025-08-08 20:46:06 +0800

    migration

---
+++ prisma/migrations/20250808124555_item_optionals/migration.sql
@@ -0,0 +1,9 @@
+-- DropForeignKey
+ALTER TABLE "Item" DROP CONSTRAINT "Item_materialId_fkey";
+
+-- AlterTable
+ALTER TABLE "Item" ALTER COLUMN "materialId" DROP NOT NULL,
+ALTER COLUMN "brand" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Item" ADD CONSTRAINT "Item_materialId_fkey" FOREIGN KEY ("materialId") REFERENCES "Material"("id") ON DELETE SET NULL ON UPDATE CASCADE;
